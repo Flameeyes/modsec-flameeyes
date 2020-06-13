0e64f24d90641925f1cf84707274c6f214c6c61a
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-06-13 16:28:59 +0100

    Update email address.

--- verify.rb
+++ verify.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2012 Diego Elio Pettenò <flameeyes@flameeyes.eu>
+# Copyright © 2012 Diego Elio Pettenò <flameeyes@flameeyes.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
